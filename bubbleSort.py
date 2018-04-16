57bdd65b74709e4a615db378b9a21c1333bce29c
rozbujnik1 <34269867+rozbujnik1@users.noreply.github.com>
2018-04-16 12:30:51 +0200

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,6 +6,6 @@ def bubbleSort(alist):
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 
-alist = [54,26,93,17,77,31,44,55,20]
+alist = [54,26,93,17,77,31,44,55,20,43,45]
 bubbleSort(alist)
 print(alist)
